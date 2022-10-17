1304d2467db4cd0ed92216796b6cb4ca29f53a0c
2W2-PalaciosLucio-112035 <112035@tecnicatura.frc.utn.edu.ar>
2022-10-17 10:12:05 -0300

    Revisar por posibles errores

--- Web.Api.Softijs/Commands/ComandoLogin.cs
+++ Web.Api.Softijs/Commands/ComandoLogin.cs
@@ -1,8 +1,13 @@
-﻿namespace Web.Api.Softijs.Commands
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Api.Softijs.Commands
 {
     public class ComandoLogin
     {
+        [Required(ErrorMessage ="El email es requerido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="La contrseña es requerida")]
         public string Contrasenia { get; set; }
     }
 }
--- Web.Api.Softijs/Results/Login/ResultadoLogin.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Web.Api.Softijs.Results.Login
-{
-    public class ResultadoLogin: ResultadoBase
-    {
-        public bool resultadoLogin { get; set; } 
-    }
-}
