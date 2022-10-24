e9b3c06d1fb6d42556875fc690b31fff7870e16b
Juan Pablo Ortiz <ortizjup@outlook.com>
2022-10-24 13:53:46 -0300

    Mas Cambios Para Ventas.

---
+++ Web.Api.Softijs/Commands/Ventas/ResultadoFactura.cs
@@ -0,0 +1,10 @@
+﻿using Web.Api.Softijs.Results;
+
+namespace Web.Api.Softijs.Commands.Ventas
+{
+    public class ResultadoFactura : ResultadoBase
+    {
+        public int NroPedido { get; set; }
+        public byte[] File { get; set; }
+    }
+}
---
+++ Web.Api.Softijs/Services/Ventas/ServicioFacturas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Web.Api.Softijs.Comun.PDF;
+using Web.Api.Softijs.DataContext;
+
+namespace Web.Api.Softijs.Services.Ventas
+{
+    public class ServicioFacturas : IServicioFacturas
+    {
+        private readonly SoftijsDevContext _context;
+
+        public ServicioFacturas(SoftijsDevContext context)
+        {
+            _context = context;
+        }
+
+        public byte[] CreateFactura(int nroPedido)
+        {
+            return new byte[3];
+        }
+    }
+}
